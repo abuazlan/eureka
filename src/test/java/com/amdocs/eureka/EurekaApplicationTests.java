cc847adb0a290c32bdf9a412f71fe9d6174476e9
AbuAzlan <abid.linux@gmail.com>
2024-07-28 21:51:06 +0530

    Update EurekaApplicationTests.java

--- src/test/java/com/amdocs/eureka/EurekaApplicationTests.java
+++ src/test/java/com/amdocs/eureka/EurekaApplicationTests.java
@@ -1,5 +1,7 @@
 package com.amdocs.eureka;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,25 @@ class EurekaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void intEquals() {
+
+		System.out.println("Hello World");
+		assertEquals(1, 1);
+	}
+
+	@Test
+	void stringEquals() {
+		System.out.println("Hello World");
+		assertEquals("Hello World", "Hello World");
+	}
+
+	@Test
+	void booleanEquals() {
+		System.out.println("Hello World");
+		assertEquals(true, true);
 	}
 
 }
